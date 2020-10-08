41c6802c98b16815c1ad03c0fcdcb159c2095727
Sex Instruktor <vasin.aa@phystech.edu>
2020-10-08 19:06:17 +0300

    made changes

--- noeps.h
+++ noeps.h
@@ -3,49 +3,55 @@
 
 // # = eps
 
-void dfs_noeps(int v, const automat& aut, std::vector<bool>& was, std::set<edge, comp_for_edges>& edges, bool& term) {
+void  bfs_noeps(Automat& new_automat, const Automat& aut) {
+
+    for (int current_v = 0; current_v < aut.states; ++current_v) {
+
+        std::set<edge, comp_for_edges> edges;
+        std::vector<bool> was(aut.states, false);
     
-    was[v] = true;
+        std::queue<int> queue;
+        was[current_v] = true;
+        bool is_terminal = false;
+        queue.push(current_v);
 
-    term |= aut.terminal[v];
+        while (!queue.empty()) {
+            int vertex = queue.front();
+            queue.pop();
+            is_terminal |= aut.terminal[vertex];
 
-    for (const edge& edge: aut.graph[v]) {
-        
-        if (edge.letter != '#') {
-            edges.insert(edge);
-            continue;
+            for (const edge& edge: aut.graph[vertex]) {
+                if (edge.letter != '#') {
+                    edges.insert(edge);
+                    continue;
+                }
+
+                if (was[edge.to] ) {
+                    continue;
+                }
+                
+                queue.push(edge.to);
+
+            }
         }
-        
-        if (was[edge.to] ) {
-            continue;
+
+        if (is_terminal) {
+            new_automat.add_terminal(current_v);
         }
 
-        dfs_noeps(edge.to, aut, was, edges, term);
+        for (const edge& edge: edges) {
+            new_automat.add_edge(current_v, edge);
+        }
     }
 
 }
 
-
-automat get_noeps(const automat& aut) {
+Automat get_noeps(const Automat& aut) {
     
-    automat new_automat;
-    new_automat.resize(aut.n);
-
-    for (int i = 0; i < aut.n; ++i) {
-        std::set<edge, comp_for_edges> edges;
-        std::vector<bool> was(aut.n, false);
-        bool vterm = false;
-        dfs_noeps(i, aut, was, edges, vterm);
-
-        for (const edge& edge: edges) {
-           new_automat.add_edge(i, edge);
-        }
-        
-        if (vterm) {
-            new_automat.add_terminal(i);
-        }
-    }
+    Automat new_automat;
+    new_automat.resize(aut.states);
     
+    bfs_noeps(new_automat, aut);
     new_automat.renumber();
 
     return new_automat;
