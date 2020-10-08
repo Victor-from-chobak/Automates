895242c6406baa8d7109ffccf8dde770fa7760ea
Sex Instruktor <vasin.aa@phystech.edu>
2020-10-08 19:27:38 +0300

    changed

--- test.cpp
+++ test.cpp
@@ -4,8 +4,8 @@
 #include "noeps.h"
 #include "determinate.h"
 
-automat get_data(std::string& file_name) {
-    automat answer;
+Automat get_data(std::string& file_name) {
+    Automat answer;
     std::ifstream fin(file_name.c_str());
     fin >> answer;
     return answer;
@@ -14,14 +14,14 @@ automat get_data(std::string& file_name) {
 
 TEST(min_test1, simple_test) {
     std::string file_name = "./tests/min_test1.in";
-    automat aut = get_min_automate(get_data(file_name));
+    Automat aut = get_min_automate(get_data(file_name));
     int n = 2;
     int m = 2;
     int t = 1;
     Graph g = {{{1, 'a'}}, {{1, 'a'}}} ;
     std::vector<bool> term = {0, 1};
     
-    automat answer(n, m, t, g, term);
+    Automat answer(n, m, t, g, term);
 
     EXPECT_EQ(aut, answer);
 }
@@ -29,7 +29,7 @@ TEST(min_test1, simple_test) {
 TEST(min_test2, nothing_to_do_test) {
    
     std::string file_name = "./tests/min_test2.in";
-    automat aut = get_min_automate(get_data(file_name));
+    Automat aut = get_min_automate(get_data(file_name));
     Graph g = {{{1, 'a'}, {1, 'b'}}, {{1, 'a'}, {1, 'b'}}};
     std::vector<bool> term = {0, 1};
 
@@ -37,7 +37,7 @@ TEST(min_test2, nothing_to_do_test) {
     int m = 4;
     int t = 1;
 
-    automat answer(n, m, t, g, term);
+    Automat answer(n, m, t, g, term);
 
     EXPECT_EQ(aut, answer);
 }
@@ -45,7 +45,7 @@ TEST(min_test2, nothing_to_do_test) {
 TEST(min_test3, full_graph_test) {
     
     std::string file_name = "./tests/min_test3.in";
-    automat aut = get_min_automate(get_data(file_name));
+    Automat aut = get_min_automate(get_data(file_name));
     Graph g = {{{1, 'a'}, {0, 'b'}}, {{1, 'a'}, {0, 'b'}}};
     std::vector<bool> term = {0, 1};
 
@@ -53,7 +53,7 @@ TEST(min_test3, full_graph_test) {
     int m = 4;
     int t = 1;
 
-    automat answer(n, m, t, g, term);
+    Automat answer(n, m, t, g, term);
 
     EXPECT_EQ(aut, answer);
 }
@@ -61,7 +61,7 @@ TEST(min_test3, full_graph_test) {
 TEST(min_test4, small_fda) {
 
     std::string file_name = "./tests/min_test4.in";
-    automat aut = get_min_automate(get_data(file_name));
+    Automat aut = get_min_automate(get_data(file_name));
     Graph g = {{{0, 'a'}, {1, 'b'} }, {{2, 'a'}, {1, 'b'}}, {{0, 'a'}, {3, 'b'}}, {{3, 'a'}, {3, 'b'}}};
     std::vector<bool> term = {0, 0, 0, 1};
     
@@ -69,7 +69,7 @@ TEST(min_test4, small_fda) {
     int m = 8;
     int t = 1;
     
-    automat answer(n, m, t, g, term);
+    Automat answer(n, m, t, g, term);
 
     EXPECT_EQ(aut, answer);
 }
@@ -77,7 +77,7 @@ TEST(min_test4, small_fda) {
 TEST(min_test5, good_fda) {
 
     std::string file_name = "./tests/min_test5.in";
-    automat aut = get_min_automate(get_data(file_name));
+    Automat aut = get_min_automate(get_data(file_name));
     Graph g = {{{1, 'a'}, {0, 'b'} }, {{1, 'a'}, {2, 'b'}}, {{2, 'a'}, {3, 'b'}}, {{0, 'a'}, {2, 'b'}}};
     std::vector<bool> term = {0, 0, 1, 1};
     
@@ -85,7 +85,7 @@ TEST(min_test5, good_fda) {
     int m = 8;
     int t = 2;
     
-    automat answer(n, m, t, g, term);
+    Automat answer(n, m, t, g, term);
 
     EXPECT_EQ(aut, answer);
 }
@@ -94,7 +94,7 @@ TEST(min_test5, good_fda) {
 TEST(noeps_test1, simple_test) {
 
     std::string file_name = "./tests/noeps_test1.in";
-    automat aut = get_noeps(get_data(file_name));
+    Automat aut = get_noeps(get_data(file_name));
     Graph g = { {} };
     std::vector<bool> term = {1};
     
@@ -102,7 +102,7 @@ TEST(noeps_test1, simple_test) {
     int m = 0;
     int t = 1;
 
-    automat answer(n, m, t, g, term);
+    Automat answer(n, m, t, g, term);
 
     EXPECT_EQ(aut, answer);
 }
@@ -110,7 +110,7 @@ TEST(noeps_test1, simple_test) {
 TEST(noeps_test2, adding_edge_and_terminate) {
 
     std::string file_name = "./tests/noeps_test2.in";
-    automat aut = get_noeps(get_data(file_name));
+    Automat aut = get_noeps(get_data(file_name));
     Graph g = {{ {1, 'a'}, {2, 'b'} }, {{3, 'a'} }, { {4, 'c'} }, {}, {} };
     std::vector<bool> term = {0, 1, 0, 0, 1};
     
@@ -118,7 +118,7 @@ TEST(noeps_test2, adding_edge_and_terminate) {
     int m = 4;
     int t = 2;
 
-    automat answer(n, m, t, g, term);
+    Automat answer(n, m, t, g, term);
 
     EXPECT_EQ(answer, aut);
 }
@@ -126,7 +126,7 @@ TEST(noeps_test2, adding_edge_and_terminate) {
 TEST(noeps_test3, only_one_normal_edge) {
 
     std::string file_name = "./tests/noeps_test3.in";
-    automat aut = get_noeps(get_data(file_name));
+    Automat aut = get_noeps(get_data(file_name));
     Graph g = { { {1, 'a'} }, {} } ;
     std::vector<bool> term = {1, 0};
 
@@ -134,14 +134,14 @@ TEST(noeps_test3, only_one_normal_edge) {
     int m = 1;
     int t = 1;
 
-    automat answer(n, m, t, g, term);
+    Automat answer(n, m, t, g, term);
 
     EXPECT_EQ(aut, answer);
 }
 
 TEST(noeps_test4, wihout_eps_edges) {
     std::string file_name = "./tests/det_test3.in";
-    automat aut = get_noeps(get_data(file_name));
+    Automat aut = get_noeps(get_data(file_name));
     Graph g = { {{0, 'a'}, {1, 'a'}, {2, 'a'}}, {{3, 'b'}}, {{3, 'b'}}, {{3, 'a'}, {4, 'a'}}, {{1, 'a'}, {5, 'b'}}, {{3, 'b'}} };
     std::vector<bool> term = {0, 0, 1, 0, 0, 1};
 
@@ -149,7 +149,7 @@ TEST(noeps_test4, wihout_eps_edges) {
     int m = 10;
     int t = 2;
 
-    automat answer(n, m, t, g, term);
+    Automat answer(n, m, t, g, term);
 
     EXPECT_EQ(aut, answer);
 }
@@ -157,7 +157,7 @@ TEST(noeps_test4, wihout_eps_edges) {
 TEST(noeps_test5, good_test_with_reduced_vertex) {
     
     std::string file_name = "./tests/noeps_test4.in";
-    automat aut = get_noeps(get_data(file_name));
+    Automat aut = get_noeps(get_data(file_name));
     Graph g = { {{0, 'a'}, {1, 'b'}, {2, 'a'}, {2, 'b'}, {3, 'b'}}, {{0, 'a'}, {1, 'b'}, {2, 'a'}, {2, 'b'}, {3, 'b'}}, {{0, 'a'}, {2, 'a'}, {2, 'b'}, {3, 'b'}}, {{2, 'a'}} };
     std::vector<bool> term = {1, 1, 1, 1};
 
@@ -165,7 +165,7 @@ TEST(noeps_test5, good_test_with_reduced_vertex) {
     int m = 15;
     int t = 4;
 
-    automat answer(n, m, t, g, term);
+    Automat answer(n, m, t, g, term);
 
     EXPECT_EQ(aut, answer);
 }
@@ -174,7 +174,7 @@ TEST(noeps_test5, good_test_with_reduced_vertex) {
 TEST(det_test1, simple_test) {
 
     std::string file_name = "./tests/det_test1.in";
-    automat aut = determinate(get_data(file_name));
+    Automat aut = determinate(get_data(file_name));
     Graph g = { { {1, 'a'} }, {{1, 'b'} } } ;
     std::vector<bool> term = {0, 1};
 
@@ -182,7 +182,7 @@ TEST(det_test1, simple_test) {
     int m = 2;
     int t = 1;
 
-    automat answer(n, m, t, g, term);
+    Automat answer(n, m, t, g, term);
 
     EXPECT_EQ(aut, answer);
 }
@@ -191,7 +191,7 @@ TEST(det_test1, simple_test) {
 TEST(det_test2, already_determined) {
 
     std::string file_name = "./tests/det_test2.in";
-    automat aut = determinate(get_data(file_name));
+    Automat aut = determinate(get_data(file_name));
     Graph g = { { {1, 'a'}, {2, 'b'}}, {{0, 'a'}, {2, 'b'}}, {{3, 'a'}}, {{1, 'a'}, {2, 'b'} }};
     std::vector<bool> term = {0, 1, 1, 0};
     
@@ -199,7 +199,7 @@ TEST(det_test2, already_determined) {
     int m = 7;
     int t = 2;
 
-    automat answer(n, m, t, g, term);
+    Automat answer(n, m, t, g, term);
 
     EXPECT_EQ(aut, answer);
 }
@@ -208,7 +208,7 @@ TEST(det_test2, already_determined) {
 TEST(det_test3, random_nka) {
 
     std::string file_name = "./tests/det_test3.in";
-    automat aut = determinate(get_data(file_name));
+    Automat aut = determinate(get_data(file_name));
     Graph g = { { {1, 'a'} }, {{1, 'a'}, {2, 'b'}}, {{3, 'a'}}, {{4, 'a'}, {5, 'b'}}, {{4, 'a'}, {6, 'b'}}, {{2, 'b'}}, {{3, 'a'}, {2, 'b'}} };
     std::vector<bool> term = {0, 1, 0, 0, 0, 1, 1};
     
@@ -216,7 +216,7 @@ TEST(det_test3, random_nka) {
     int m = 11;
     int t = 3;
 
-    automat answer(n, m, t, g, term);
+    Automat answer(n, m, t, g, term);
 
     EXPECT_EQ(aut, answer);
 }
@@ -224,7 +224,7 @@ TEST(det_test3, random_nka) {
 TEST(det_test4, random_nka) {
 
     std::string file_name = "./tests/det_test4.in";
-    automat aut = determinate(get_data(file_name));
+    Automat aut = determinate(get_data(file_name));
     Graph g = { { {1, 'a'}, {2, 'b'} }, { {1, 'a'}, {2, 'b'} }, { {3, 'a'} },{ {4, 'a'}, {5, 'b'}}, {{3, 'a'}}, {{4, 'a'}, {2, 'b'}} };
     std::vector<bool> term = {0, 0, 1, 1, 1, 0};
     
@@ -232,7 +232,7 @@ TEST(det_test4, random_nka) {
     int m = 10;
     int t = 3;
 
-    automat answer(n, m, t, g, term);
+    Automat answer(n, m, t, g, term);
 
     EXPECT_EQ(aut, answer);
 }
@@ -240,7 +240,7 @@ TEST(det_test4, random_nka) {
 TEST(det_test5, exponential_growth) {
 
     std::string file_name = "./tests/det_test5.in";
-    automat aut = determinate(get_data(file_name));
+    Automat aut = determinate(get_data(file_name));
     Graph g = { { {1, 'a'}, {0, 'b'} }, { {2, 'a'}, {3, 'b'} }, { {4, 'a'}, {5, 'b'} }, { {6, 'a'}, {7, 'b'} }, { {4, 'a'}, {5, 'b'} }, 
                 { {6, 'a'}, {7, 'b'} }, { {2, 'a'}, {3, 'b'} }, { {1, 'a'}, {0, 'b'} } };
     
@@ -250,7 +250,7 @@ TEST(det_test5, exponential_growth) {
     int m = 16;
     int t = 4;
 
-    automat answer(n, m, t, g, term);
+    Automat answer(n, m, t, g, term);
 
     EXPECT_EQ(aut, answer);
 }
